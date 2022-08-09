b2b04f3d07c34402c052f551a83e958b7fdf92b8
Mrkelles <kelvinebima@gmail.com>
2022-08-09 16:23:53 -0700

    Initial commit

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,4 +1,4 @@
-
+import 'default.scss'
 
 function App() {
   return (
---
+++ shop/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = () => {
+  return (
+    <header className="">
+      
+    </header>
+  )
+}
+
+export default Header;
